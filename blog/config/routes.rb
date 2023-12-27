e5090c669ecd1dc6e85bbc68bf099a250b5c5d6e
Alex Oswalt <oswaltalex@gmail.com>
2023-12-27 15:51:22 -0800

    Added route for comments
    Up to https://guides.rubyonrails.org/getting_started.html#generating-a-controller

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-	root "articles#index"
-	resources :articles
+  root "articles#index"
+  
+  resources :articles do
+    resources :comments
+  end
+end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
